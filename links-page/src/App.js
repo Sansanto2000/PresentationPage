28857ce59f0191aea3494c2d7d21db7b0cd6d951
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-03-04 20:03:46 -0300

    :sparkles: Incorporacion de Titulo y Subtitulo

--- links-page/src/App.js
+++ links-page/src/App.js
@@ -1,10 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import links from "./data/links.json";
 
 function App() {
+  console.log(links);
   return (
     <div className="App">
       <img src={"fotoSAPonteAhon.jpg"} className="User-Photo" alt="Mi foto" />
+      <h1 className="Title"> S. A. Ponte Ahón</h1>
+      <h2 className="Subtitle">Lic. Informática - Lic. Sistemas</h2>
     </div>
   );
 }
